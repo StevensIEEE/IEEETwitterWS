bd98f96afffd0df40bf868c4b4bb5476c948fdee
Izy Engel <isabelle.engel@gmail.com>
2021-02-24 12:55:39 -0500

    added pulling images from tweets

--- pullTweets.py
+++ pullTweets.py
@@ -1,4 +1,5 @@
 import tweepy
+import wget 
 
 #Import authentication keys
 from auth import (
@@ -15,13 +16,27 @@ auth.set_access_token(access_token, access_token_secret)
 #Create object twitter of Twython and pass in keys
 twitter = tweepy.API(auth)
 
-timeline = twitter.home_timeline()
-print(timeline)
+#timeline = twitter.home_timeline()
+#print(timeline)
 
-#tweets = twitter.user_timeline('@bodega_cats')
+
+tweets = twitter.user_timeline('@Bodegacats_')
 #print(tweets)
 
+media_files = set()
+for status in tweets:
+    media = status.entities.get('media', [])
+    if(len(media) > 0):
+        media_files.add(media[0]['media_url'])
 
-#print(tweet[0].text)
+for media_file in media_files:
+    wget.download(media_file)
+
+#print(tweets[0].text)
 #for tweet in tweets:
 #    print(tweet.text)
+
+
+followers = twitter.followers()[0].name
+print("My followers: {}".format(followers))
+
