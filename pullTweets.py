7648619178954145d008a8f9541f92a8743200da
bmirisola <bennymirisola@gmail.com>
2021-10-03 23:32:06 -0400

    added downloading media

--- pullTweets.py
+++ pullTweets.py
@@ -1,5 +1,5 @@
 import tweepy
-import wget 
+import wget
 
 #Import authentication keys
 from auth import (
@@ -10,18 +10,21 @@ from auth import (
 )
 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)   
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 #Create object twitter of Twython and pass in keys
 twitter = tweepy.API(auth)
 
+#uncomment for to get json description of home timeline. Mostly useless for us
 #timeline = twitter.home_timeline()
 #print(timeline)
 
 
-tweets = twitter.user_timeline('@Bodegacats_')
-#print(tweets)
+tweets = twitter.user_timeline(screen_name='@memeadikt')
+
+#prints tweets for user specified
+print(tweets)
 
 media_files = set()
 for status in tweets:
@@ -32,11 +35,11 @@ for status in tweets:
 for media_file in media_files:
     wget.download(media_file)
 
+
+
 #print(tweets[0].text)
 #for tweet in tweets:
 #    print(tweet.text)
 
-
-followers = twitter.followers()[0].name
-print("My followers: {}".format(followers))
-
+#followers = twitter.followers()[0].name
+#print("My followers: {}".format(followers))
