2b4ebf6087033b9c599ebffd3b1901f9dd0dd5fe
bmirisola <bennymirisola@gmail.com>
2021-10-03 21:49:18 -0400

    added comments

--- tweepyTweet.py
+++ tweepyTweet.py
@@ -1,6 +1,5 @@
 import tweepy
 
-
 #Import authentication keys
 from auth import (
     consumer_key,
@@ -9,8 +8,10 @@ from auth import (
     access_token_secret
 )
 
+# Creates object that has permission to use your personal twitter account
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)   
+# Gives account permission to use Twitter API
 auth.set_access_token(access_token, access_token_secret)
 
 #Create object twitter of Twython and pass in keys
@@ -21,8 +22,6 @@ message = "Hello IEEE!"
 #Calls update_status method in Tweepy and passes in your message
 
 twitter.update_status(status=message)
-
-
 
 #twitter.update_with_media('1200px-IEEE_logo.png', message)
 
